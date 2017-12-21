fe46f50cb5b27a43f44abff91ba98581146cedac
HanZala <hanzalasiddiqui1992@gmail.com>
2017-12-21 11:30:18 +0500

    toaster

--- BISEWEB/Controllers/AddStuffController.cs
+++ BISEWEB/Controllers/AddStuffController.cs
@@ -32,14 +32,23 @@ namespace BISEWEB.Controllers
         
 
         // GET: AddStuff
+        [HttpGet]
         public ActionResult AddEmployee()
         {
           
             return View();
 
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddEmployee(tblemployee obj)
+        {
 
-       
+            return View();
+
+        }
+
+
     }
 
 }
