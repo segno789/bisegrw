66dcb21ce4858185fb94b07dbcaa774090f3b79a
Ahsan <ahsanavais@live.com>
2018-01-01 09:15:28 +0500

    a

--- BISEWEB/Controllers/AddStuffController.cs
+++ BISEWEB/Controllers/AddStuffController.cs
@@ -25,19 +25,23 @@ namespace BISEWEB.Controllers
         [HttpGet]
         public ActionResult AddNewInst()
         {
+            ViewBag.tehsil = new SelectList(matric_new.tblTehsils.Where(o=>o.teh_cd<20), "teh_cd", "teh_name");
+            //var tehsil = from p in matric_new.tblTehsils where p.teh_cd<20 select new {Tehsilname = p.teh_name,TehsilCode = p.teh_cd};
+            //SelectList teh_list = new SelectList(tehsil, "TehsilCode", "Tehsilname");
+            //ViewBag.tehsilsss = teh_list;
 
-            var tehsil = from p in matric_new.tblTehsils where p.teh_cd<20 select new {Tehsilname = p.teh_name,TehsilCode = p.teh_cd};
-            SelectList teh_list = new SelectList(tehsil, "TehsilCode", "Tehsilname");
-            ViewBag.tehsil = teh_list;
+            ////var Izone = from p in matric_new.tblZones where p.Class  Convert.ToByte(12)  select new { ZoneName = p.zone_cd, TehsilCode = p.teh_cd };
+            ////SelectList IZone_list = new SelectList(Izone, "TehsilCode", "Tehsilname");
+            ////ViewBag.Izone = IZone_list;
 
-            //var Izone = from p in matric_new.tblZones where p.Class  Convert.ToByte(12)  select new { ZoneName = p.zone_cd, TehsilCode = p.teh_cd };
-            //SelectList IZone_list = new SelectList(Izone, "TehsilCode", "Tehsilname");
-            //ViewBag.Izone = IZone_list;
+            //var Mzone = from p in matric_new.tblTehsils select new { Tehsilname = p.teh_name, TehsilCode = p.teh_cd };
+            //SelectList MZone_list = new SelectList(Mzone, "TehsilCode", "Tehsilname");
+            //ViewBag.Izone = MZone_list;
 
-            var Mzone = from p in matric_new.tblTehsils select new { Tehsilname = p.teh_name, TehsilCode = p.teh_cd };
-            SelectList MZone_list = new SelectList(Mzone, "TehsilCode", "Tehsilname");
-            ViewBag.Izone = MZone_list;
 
+            ViewBag.Izone = new SelectList(matric_new.tblZones.Where(o => o.mYear == DateTime.Now.Year && o.Class == 12), "zone_cd", "zone_name");
+
+            ViewBag.Mzone = new SelectList(matric_new.tblZones.Where(o => o.mYear == DateTime.Now.Year && o.Class == 10), "zone_cd", "zone_name");
 
             return View();
 
@@ -67,6 +71,30 @@ namespace BISEWEB.Controllers
             return View();
 
         }
+
+        //public ActionResult GetZonesName(int zone_code )
+        //{
+        //    var zone_name = "";
+
+        //    zone
+
+        //    zone_name = matric_new.tblZones.Where(o => o.zone_cd = zone_code)    //Select(o => o.zone_name).Where(o => o);
+        //    return Json(zone_name, JsonRequestBehavior.AllowGet);
+        //}
+
+
+        //public ActionResult GetCityList(string stateID)
+        //{
+        //    List<CITy> lstcity = new List<CITy>();
+        //    int stateiD = Convert.ToInt32(stateID);
+        //    using (CITYSTATEEntities cITYSTATEEntities = new CITYSTATEEntities())
+        //    {
+        //        lstcity = (cITYSTATEEntities.CITIES.Where(x => x.StateId == stateiD)).ToList<CITy>();
+        //    }
+        //    JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+        //    string result = javaScriptSerializer.Serialize(lstcity);
+        //    return Json(result, JsonRequestBehavior.AllowGet);
+        //}
 
 
     }
